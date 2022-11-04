eb6f522385eaf4f7451641f3467ccd564a10a2cd
Nap Joseph Calub <njncalub@gmail.com>
2022-11-05 00:24:48 +0800

    docs(copyright): update footer with links

--- components/ui/SiteFooter.tsx
+++ components/ui/SiteFooter.tsx
@@ -7,16 +7,69 @@ export default function SiteFooter() {
           <a
             href="https://wanderinginn.com/"
             class="font-semibold text-[#1583af] hover:underline"
+            target="_blank"
           >
             The Wandering Inn
-          </a>
-          , a universe by{" "}
+          </a>.
+        </span>{" "}
+        <span>
+          Copyright for almost all the text in this repository goes to the
+          author,{" "}
           <a
-            href="https://www.patreon.com/pirateaba"
+            href="https://patreon.com/pirateaba"
             class="font-semibold text-[#1583af] hover:underline"
+            target="_blank"
           >
             pirateaba
           </a>.
+        </span>{" "}
+        <span>
+          Please support them on their official channels:{" "}
+          <span>
+            <a
+              href="https://wanderinginn.com/"
+              class="font-semibold text-[#1583af] hover:underline"
+              target="_blank"
+            >
+              The Wandering Inn website
+            </a>
+          </span>,{" "}
+          <span>
+            <a
+              href="https://reddit.com/r/WanderingInn/"
+              class="font-semibold text-[#1583af] hover:underline"
+              target="_blank"
+            >
+              The Wandering Inn subreddit
+            </a>
+          </span>,{" "}
+          <span>
+            <a
+              href="https://patreon.com/pirateaba"
+              class="font-semibold text-[#1583af] hover:underline"
+              target="_blank"
+            >
+              Patreon
+            </a>
+          </span>,{" "}
+          <span>
+            <a
+              href="https://twitch.tv/pirateaba"
+              class="font-semibold text-[#1583af] hover:underline"
+              target="_blank"
+            >
+              Twitch
+            </a>
+          </span>,{" and "}
+          <span>
+            <a
+              href="https://youtube.com/user/pirateaba"
+              class="font-semibold text-[#1583af] hover:underline"
+              target="_blank"
+            >
+              YouTube
+            </a>
+          </span>.
         </span>
       </nav>
     </>
